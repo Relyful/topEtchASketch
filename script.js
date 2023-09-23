9558b06fef7716cf0c199eca76f30339af1db9fb
Relyful <13381270+Relyful@users.noreply.github.com>
2023-09-24 01:14:08 +0200

    Creat basic layout and functions for etch-a-sketch. Achieve basic functionality, creates grid up top 100 divs and makes backgroundColor Black on hover.

---
+++ script.js
@@ -0,0 +1,39 @@
+const drawArea = document.querySelector('.drawArea');
+const buttonko = document.querySelector('.buttonko');
+let divAmmount;
+let userSize
+
+
+buttonko.addEventListener('click', () => {
+    userSize = prompt('What size of grid would you like?');
+    (userSize > 100) ? userSize = 100 : "";
+    divAmmount = userSize * userSize;
+    drawGrid();
+})
+
+
+
+function drawGrid() {
+    let drawBox = document.querySelectorAll('.drawBox');
+
+    drawBox.forEach(item => {
+        item.remove();
+    })
+
+    
+    for (let i = 0; i < divAmmount; i++) {
+        const newDiv = document.createElement('div');
+        let calcSize = 1000/userSize;
+        newDiv.style.height = `${calcSize}px`;
+        newDiv.style.width = `${calcSize}px`;
+        newDiv.classList.add('drawBox');   
+        drawArea.appendChild(newDiv);
+    }
+
+    drawBox = document.querySelectorAll('.drawBox');
+    drawBox.forEach(item => {
+        item.addEventListener('mouseenter', (e) => {
+            e.target.style.backgroundColor = 'black';
+        })
+    })
+}
