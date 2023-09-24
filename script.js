07b2aaf97923abb84e1c2fe02795c5cd21c0c50e
Relyful <13381270+Relyful@users.noreply.github.com>
2023-09-24 21:06:02 +0200

    Add functionality of random rgb divs in addition to black divs. Create buttons in which user can chose black of rgb drawin mode.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const drawArea = document.querySelector('.drawArea');
 const buttonko = document.querySelector('.buttonko');
+const black = document.querySelector('.black');
+const random = document.querySelector('.random');
+let drawBox = document.querySelectorAll('.drawBox');
 let divAmmount;
 let userSize
 
@@ -14,7 +17,7 @@ buttonko.addEventListener('click', () => {
 
 
 function drawGrid() {
-    let drawBox = document.querySelectorAll('.drawBox');
+    drawBox = document.querySelectorAll('.drawBox');
 
     drawBox.forEach(item => {
         item.remove();
@@ -33,7 +36,35 @@ function drawGrid() {
     drawBox = document.querySelectorAll('.drawBox');
     drawBox.forEach(item => {
         item.addEventListener('mouseenter', (e) => {
-            e.target.style.backgroundColor = 'black';
+            e.target.style.backgroundColor = `black`;
         })
     })
 }
+
+random.addEventListener('click', () => {
+    drawBox = document.querySelectorAll('.drawBox');
+    drawBox.forEach(item => {
+        drawBox.forEach(item => {
+            item.remove();
+        })})
+
+        userSize = prompt('What size of grid would you like?');
+        (userSize > 100) ? userSize = 100 : "";
+        divAmmount = userSize * userSize;
+
+        for (let i = 0; i < divAmmount; i++) {
+            const newDiv = document.createElement('div');
+            let calcSize = 800/userSize;
+            newDiv.style.height = `${calcSize}px`;
+            newDiv.style.width = `${calcSize}px`;
+            newDiv.classList.add('drawBox');   
+            drawArea.appendChild(newDiv);
+        }
+       
+        drawBox = document.querySelectorAll('.drawBox');
+        drawBox.forEach(item => {
+            item.addEventListener('mouseenter', (e) => {
+                e.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+        })
+    })
+})
